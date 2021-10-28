bf59b812096d638519acdeb94b34586aecde522f
unknown <xavierquispezamorano@gmail.com>
2021-10-27 21:57:21 -0500

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,46 @@
+<li class="nav-item">
+    <a href="{{ route('usuarios.index') }}"
+       class="nav-link {{ Request::is('usuarios*') ? 'active' : '' }}">
+        <p>Usuarios</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('pacientes.index') }}"
+       class="nav-link {{ Request::is('pacientes*') ? 'active' : '' }}">
+        <p>Pacientes</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('citas.index') }}"
+       class="nav-link {{ Request::is('citas*') ? 'active' : '' }}">
+        <p>Citas</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('citasDetalles.index') }}"
+       class="nav-link {{ Request::is('citasDetalles*') ? 'active' : '' }}">
+        <p>Citas Detalles</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('doctors.index') }}"
+       class="nav-link {{ Request::is('doctors*') ? 'active' : '' }}">
+        <p>Doctors</p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="{{ route('vuedoctors.index') }}"
+       class="nav-link {{ Request::is('vuedoctors*') ? 'active' : '' }}">
+        <p>Vue Dortor</p>
+    </a>
+</li>
+
---
+++ resources/views/vuedoctors/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="app" class="content">
+        <doctor-component></doctor-component>
+    </div>
+
+@endsection
+
+
